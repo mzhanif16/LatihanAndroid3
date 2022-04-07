1144fdb2e688d206264a99f989df67342ee4b9c5
Hanif <zakihanif155@gmail.com>
2022-04-07 17:01:55 +0700

    Latihan Android2

---
+++ app/src/main/java/ac/id/pnj/latihanandroid/tugas1/RegistActivity.java
@@ -0,0 +1,16 @@
+package ac.id.pnj.latihanandroid.tugas1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ac.id.pnj.latihanandroid.R;
+
+public class RegistActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_regist);
+    }
+}
