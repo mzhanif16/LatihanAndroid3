217249cca3f8db99134c1964820303b34bf5df80
Hanif <zakihanif155@gmail.com>
2022-04-08 12:56:54 +0700

    apa

--- app/src/main/java/ac/id/pnj/latihanandroid/tugas1/HomeActivity.java
+++ app/src/main/java/ac/id/pnj/latihanandroid/tugas1/HomeActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import ac.id.pnj.latihanandroid.R;
+
 public class HomeActivity extends AppCompatActivity {
 
     @Override
